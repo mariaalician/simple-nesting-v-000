c15a7028fc19ad9a1f611eb51fcfe347f3a842de
Maria Nunez <mariaalician@gmail.com>
2018-06-21 02:10:10 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -97,7 +97,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     [:alan_kay][:known_for]
      
 end
 
